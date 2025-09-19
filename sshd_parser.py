22b1e26454442aba6ae0cc5ca95d5acde621cd4c
supunhg <sanchithahewagamage@gmail.com>
2025-09-19 16:02:02 +0530

    Fix slight spacing error in parser

--- sshd_parser.py
+++ sshd_parser.py
@@ -49,7 +49,7 @@ class SSHDConfig:
                 "# Beware issues with some PAM modules and threads."
             ],
             [
-                "#This is the sshd server system-wide configuration file.  See",
+                "# This is the sshd server system-wide configuration file.  See",
                 "# sshd_config(5) for more information.",
                 "",
                 "# This sshd was compiled with PATH=/usr/local/bin:/usr/bin:/bin:/usr/games",
